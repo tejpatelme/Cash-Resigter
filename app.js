19bd477526dedb2a53792d7af62872f685b9132b
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-01-04 22:46:53 +0530

    feature addition complete

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 var billamount = document.querySelector("#billAmt");
 var cashamount = document.querySelector("#cashAmt");
 var button = document.querySelector("#button");
+var nextbutton = document.querySelector("#nextbtn");
+var cashamountdiv = document.querySelector(".cashamtdiv");
+var output = document.querySelector(".output");
+var outputDiv = document.querySelector(".outputdiv");
+var cell1 = document.querySelector(".cell1");
+var cell2 = document.querySelector(".cell2");
+
 
 var notes = [2000,500,100,20,10,5,1];
-var difference = 0;
-var result = "";
 
-function clickHandler() {   
-    difference = cashamount.value - billamount.value;
-    console.log(difference);
-    for(var i = 0; notes.length; i++){
-        if(difference >= notes[i]){
-            console.log(notes[i]);
-        }
-    }
-    console.log("working");
+function generateTable(note,noofnotes){
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    td1.innerHTML = noofnotes;
+    td2.innerHTML = note;
+    cell1.appendChild(td1);
+    cell2.appendChild(td2);
 }
 
+function calculatenotes(note,balance){
+    var noofnotes = Math.floor(balance/note);
+    console.log(note, noofnotes);
+    if (noofnotes >= 1) {
+        balance = balance - noofnotes*note;
+        console.log("balance ", balance);
+        generateTable(note,noofnotes);
+    }
+    return balance;
+}
+
+function showInput(){
+    cashamountdiv.style.display = "block";
+}
+
+function clickHandler() {  
+    // cell1.innerHTML = "";
+    // cell2.innerHTML = "";
+    var inputamount = parseInt(billamount.value);
+    var cashpaid = parseInt(cashamount.value);
+    var difference = cashpaid - inputamount;
+    outputDiv.style.display = "block";
+    for(var i = 0 ; i < notes.length ; i++){
+        if(difference>0){
+            difference = calculatenotes(notes[i],difference);
+        }
+    }   
+}
+
+nextbutton.addEventListener('click', showInput);
 button.addEventListener('click', clickHandler);
