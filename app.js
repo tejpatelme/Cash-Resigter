314bcb5e4bb716d11174422fb9f0ad609bd127ba
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-01-05 09:24:27 +0530

    refractor

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var output = document.querySelector(".output");
 var outputDiv = document.querySelector(".outputdiv");
 var cell1 = document.querySelector(".cell1");
 var cell2 = document.querySelector(".cell2");
-
+var message = document.querySelector(".message");
 
 var notes = [2000,500,100,20,10,5,1];
+
 
 function generateTable(note,noofnotes){
     var td1 = document.createElement('td');
@@ -33,26 +34,49 @@ function calculatenotes(note,balance){
 
 function showInput(){
     if(Number.isInteger(Number(billamount.value))>0){
+        hidemessage();
         cashamountdiv.style.display = "block";
     }
     else {
-        alert("enter a valid amount");
+        showmessage("Please enter a valid amount");
     }
     
 }
 
+function showmessage(text){
+    message.style.display = "block";
+    message.innerHTML = text;
+    outputDiv.style.display = "none";
+}
+
+function hidemessage() {
+    message.style.display = "none";
+}
+
 function clickHandler() {  
-    // cell1.innerHTML = "";
-    // cell2.innerHTML = "";
     var inputamount = parseInt(billamount.value);
     var cashpaid = parseInt(cashamount.value);
     var difference = cashpaid - inputamount;
-    outputDiv.style.display = "block";
-    for(var i = 0 ; i < notes.length ; i++){
-        if(difference>0){
-            difference = calculatenotes(notes[i],difference);
+    if(cashpaid===0){
+        showmessage("Please enter an amount greater than zero");
+    }
+    else if(cashpaid < inputamount) {
+        showmessage("Cash paid can't be less than bill amount");
+    }
+    else {
+        if(inputamount === cashpaid){
+            showmessage("No cash needs to be returned");
         }
-    }   
+        else {
+            hidemessage();
+            outputDiv.style.display = "block";
+            for (var i = 0; i < notes.length; i++) {
+                if (difference > 0) {
+                    difference = calculatenotes(notes[i], difference);
+                }
+            }
+    }
+    }
 }
 
 nextbutton.addEventListener('click', showInput);
